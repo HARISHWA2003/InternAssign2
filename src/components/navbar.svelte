0ad0f5e38b38a3da3a3761680b4bc2cc91d63eed
HARISHWA D REDDY <46115411+HARISHWA2003@users.noreply.github.com>
2023-10-05 00:21:21 +0530

    Add files via upload

---
+++ src/components/navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+	// import logo_white from '../assets/images/logo-white.svg';
+</script>
+
+<nav class="flex justify-between p-4 px-28 w-full bg-indigo-400 items-center z-10">
+	<div class="nav-links flex w-2/3 justify-between text-xl font-bold text-white">
+		<a href="/" class="text-gray-800">Home</a>
+		<a href="/privateProfile">Profile</a>
+		<a href="/internships">Internship</a>
+		<a href="/certificates">Certificate</a>
+		<!-- <a href="/testPage">Test Page</a> -->
+	</div>
+</nav>
+
+<style>
+</style>
